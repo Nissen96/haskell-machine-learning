613475de2099a5bcedccc41b185ea05848b13736
Alexander Nissen <alexander@feldbech.dk>
2019-03-12 19:03:16 +0100

    Make Item a newtype

--- FrequentPatternMining.hs
+++ FrequentPatternMining.hs
@@ -4,19 +4,24 @@ import qualified Data.Set as Set
 import Data.List
 import Data.Maybe (catMaybes)
 
---------------- Types -------------
-type Item = String
-type Itemset = Set.Set Item
-type Transaction = Itemset
-type Database = [Transaction]
 
-data Rule = Rule {
-    antecedent :: Itemset,
-    consequent :: Itemset
+--------------- Types -------------
+newtype Item a = Item a deriving (Eq, Ord)
+instance Show a => Show (Item a) where
+    show (Item a) = show a
+
+type Itemset a = Set.Set (Item a)
+type Transaction a = Itemset a
+type Database a = [Transaction a]
+
+data Rule a = Rule {
+    antecedent :: Itemset a,
+    consequent :: Itemset a
 } deriving (Eq, Ord)
 
-instance Show Rule where
+instance Show a => Show (Rule a) where
     show (Rule x y) = showSet x ++ " => " ++ showSet y
+
 
 ----------- Helpers ---------------
 showSet :: Show a => Set.Set a -> String
@@ -37,7 +42,7 @@ printListOfSets ls = putStrLn $ showListOfSets ls
 printSetOfSets :: Show a => Set.Set (Set.Set a) -> IO ()
 printSetOfSets set = putStrLn $ showSetOfSets set
 
-printDatabase :: Database -> IO ()
+printDatabase :: Show a => Database a -> IO ()
 printDatabase db = putStrLn $ "DB = " ++ showListOfSets db
 
 toRealDiv :: (Integral a, Fractional b) => a -> a -> b
@@ -58,45 +63,48 @@ powerSet set
             powset = powerSet xs
 
 
-
 ---------------- Itemsets ----------------
-cover :: Itemset -> Database -> Set.Set Transaction
+createItemset :: Ord a => [a] -> Itemset a
+createItemset ls = Set.fromList [Item x | x <- ls]
+
+cover :: Ord a => Itemset a -> Database a -> Set.Set (Transaction a)
 cover itemset database = Set.fromList [trans | trans <- database, itemset `Set.isSubsetOf` trans]
 
-support :: Itemset -> Database -> Int
+support :: Ord a => Itemset a -> Database a -> Int
 support itemset database = length $ cover itemset database
 
-frequency :: Itemset -> Database -> Float
+frequency :: Ord a => Itemset a -> Database a -> Float
 frequency itemset database = (support itemset database) `toRealDiv` (length database)
 
-isFrequent :: Itemset -> Database -> Int -> Bool
+isFrequent :: Ord a => Itemset a -> Database a -> Int -> Bool
 isFrequent itemset database threshold = (support itemset database) >= threshold
 
-extractItems :: Database -> Itemset
+extractItems :: Ord a => Database a -> Itemset a
 extractItems database = Set.unions database
 
+
 ------------------ Rules ------------------
-ruleUnion :: Rule -> Itemset
+ruleUnion :: Ord a => Rule a -> Itemset a
 ruleUnion rule = antecedent rule `Set.union` consequent rule
 
-ruleSupport :: Rule -> Database -> Int
+ruleSupport :: Ord a => Rule a -> Database a -> Int
 ruleSupport rule database = support (ruleUnion rule) database
 
-ruleFrequency :: Rule -> Database -> Float
+ruleFrequency :: Ord a => Rule a -> Database a -> Float
 ruleFrequency rule database = frequency (ruleUnion rule) database
 
-confidence :: Rule -> Database -> Float
+confidence :: Ord a => Rule a -> Database a -> Float
 confidence rule database = (ruleSupport rule database) `toRealDiv` (support (antecedent rule) database)
 
 
 -------------------- Apriori ------------------
 
 
-generateCandidates :: Set.Set Itemset -> Set.Set Itemset
+generateCandidates :: Ord a => Set.Set (Itemset a) -> Set.Set (Itemset a)
 generateCandidates freqSets = Set.fromList $ catMaybes $ map (\(x, y) -> join x y) $ cartesianNonreflexive sorted sorted
     where sorted = map (sort . Set.toList) $ Set.toList freqSets
 
-join :: [Item] -> [Item] -> Maybe Itemset
+join :: Ord a => [Item a] -> [Item a] -> Maybe (Itemset a)
 join p q
     | preP == preQ = Just $ (Set.fromList p) `Set.union` (Set.fromList q)
     | otherwise    = Nothing
--- Main.hs
+++ Main.hs
@@ -10,22 +10,22 @@ main = do
     let eggs = "eggs"
     let salt = "salt"
 
-    putStrLn butter
-    putStrLn bread
-    putStrLn sugar
+    print butter
+    print bread
+    print sugar
 
-    let t1 = Set.fromList [bread, butter, milk, sugar]
-    let t2 = Set.fromList [butter, flour, milk, sugar]
-    let t3 = Set.fromList [butter, eggs, milk, salt]
-    let t4 = Set.fromList [eggs]
-    let t5 = Set.fromList [butter, flour, milk, salt, sugar]
+    let t1 = createItemset [bread, butter, milk, sugar]
+    let t2 = createItemset [butter, flour, milk, sugar]
+    let t3 = createItemset [butter, eggs, milk, salt]
+    let t4 = createItemset [eggs]
+    let t5 = createItemset [butter, flour, milk, salt, sugar]
     printSet t1
 
     let db = [t1, t2, t3, t4, t5]
 
     printDatabase db
 
-    let test = Set.fromList [milk, sugar, butter]
+    let test = createItemset [milk, sugar, butter]
 
     printSetOfSets $ cover test db
     print $ support test db
@@ -34,8 +34,8 @@ main = do
     print $ isFrequent test db 3
     printSetOfSets $ powerSet $ Set.fromList [1, 2, 3, 4]
 
-    let ant = Set.fromList [butter, milk]
-    let cons = Set.fromList [salt]
+    let ant = createItemset [butter, milk]
+    let cons = createItemset [salt]
     let rule = Rule ant cons
     print rule
 
@@ -47,17 +47,15 @@ main = do
     printSet items
     printSetOfSets $ powerSet items
 
-    let i1 = "1"
-    let i2 = "2"
-    let i3 = "3"
-    let i4 = "4"
-    let i5 = "5"
+    let s1 = createItemset [1, 2, 3]
+    let s2 = createItemset [1, 2, 4]
+    let s3 = createItemset [1, 3, 4]
+    let s4 = createItemset [1, 3, 5]
+    let s5 = createItemset [2, 3, 4]
 
-    let s1 = Set.fromList [i1, i2, i3]
-    let s2 = Set.fromList [i1, i2, i4]
-    let s3 = Set.fromList [i1, i3, i4]
-    let s4 = Set.fromList [i1, i3, i5]
-    let s5 = Set.fromList [i2, i3, i4]
+    printSet s1
 
     let freqSets = Set.fromList [s1, s2, s3, s4, s5]
-    printSetOfSets $ generateCandidates freqSets
+    printSetOfSets freqSets
+    let candidates = generateCandidates freqSets
+    printSetOfSets candidates
